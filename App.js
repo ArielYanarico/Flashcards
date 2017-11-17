233aa03c481a501b0a6c8a1dcee7197aa530551d
Ariel Yanarico <argos.yo@gmail.com>
2017-11-17 19:39:31 -0400

    First Commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Ups, it looks like you dont have any deck!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
